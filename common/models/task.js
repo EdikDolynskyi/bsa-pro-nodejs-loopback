7aa06d2471931413692682cd3f2ad31fff8540c7
Eduard Dolynskyi <eduard@Esther-Aidas-MacBook-Pro.local>
2017-02-10 00:31:38 +0200

    add validation via operation hooks

--- common/models/task.js
+++ common/models/task.js
@@ -3,10 +3,29 @@
 var path = require('path');
 var socket = require(path.join(__dirname, '../services/Socket'));
 
+var validateRequest = function(data){
+	if(data && data.description){
+		if(data.description.toLowerCase().indexOf('зрада') !== -1){
+			return false;
+		}
+	}
+	return true;
+};
+
 module.exports = function(Task) {
+	Task.observe('before save', function(ctx, next) {
+		if(!validateRequest(ctx.instance)){
+			var error = new Error();
+			error.status = 500;
+			next(error);
+		} else {
+			next();	
+		}
+	});
+
 	Task.observe('after save', function(ctx, next) {
 		socket.emit('updateTasksList');
-		next();
+		next();	
 	});
 
 	Task.observe('after delete', function(ctx, next) {
